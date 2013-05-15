0de7e8884a5c583fc9fc85bf0ff5574c10c07f3f
Talles Dantas <tdantas@avenuecode.com>
2013-05-15 19:57:43 -0300

    yeah

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+
+  end
 
   def show
     @user = User.find(params[:id])
@@ -11,7 +17,7 @@ class UsersController < ApplicationController
   def create
     userForm = params[:user]
     @user = User.new(name: userForm[:name], email: userForm[:email], 
-                    password: userForm[:password], password_confirmation: userForm[:password_confirmation])    # Not the final implementation!
+                     password: userForm[:password], password_confirmation: userForm[:password_confirmation])    
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -21,11 +27,40 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
-end
 
+  # Before filters
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+end
