e9420e8c473dbabf467a9e300975a52ad09aaa30
Talles Dantas <tdantas@avenuecode.com>
2013-05-16 18:14:05 -0300

    Finished 11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id",
+    class_name:  "Relationship",
+    dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
+
   before_save { self.email = email.downcase }
   before_save :create_remember_token
 
@@ -14,6 +21,16 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
 
   private
 
